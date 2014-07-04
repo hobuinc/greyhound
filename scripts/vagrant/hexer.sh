b6593b706f35c1a0b2b9b5fb89bf127002a1ec64
Connor Manning <connor@hobu.co>
2014-07-04 10:47:09 -0500

    Temporarily switch to a fork of hexer that builds for Ubuntu pending resolution of an issue.

--- scripts/vagrant/hexer.sh
+++ scripts/vagrant/hexer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 # Installs hexer library
 
-git clone https://github.com/hobu/hexer.git
+# TODO: Temporarily use this fork due to https://github.com/hobu/hexer/issues/13
+git clone https://github.com/connormanning/hexer.git
 cd hexer
-# TODO
-git checkout 8bd826d395120c6da809b95f04927b97ad77da53
 cmake . -DCMAKE_INSTALL_PREFIX=/usr
 make
 sudo make install
