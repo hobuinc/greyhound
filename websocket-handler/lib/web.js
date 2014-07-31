bf9a5c2bf1a760a31d32a5e952ad62f1b7d8e511
Connor Manning <connor@hobu.co>
2014-07-31 10:42:43 -0500

    Use a dummy on('data') handler before error validation so that the real one only runs for valid requests.

--- websocket-handler/lib/web.js
+++ websocket-handler/lib/web.js
@@ -38,11 +38,29 @@ var
 		var r = http.request(options, function(res) {
 			res.setEncoding('utf8');
 
+            var contentLength = parseInt(res.headers['content-length']);
+
             // We must have a res.on('data') function declared *before* any
             // possible early-return callbacks.  Otherwise, the response will
             // not be consumed if an error occurs, and the HTTP agent will
             // keep this connection open, causing new requests to be delayed
             // until one of them times out.
+            res.on('data', function(c) { });
+
+			if (res.statusCode / 100 !== 2) {
+				return cb(new Error(
+                        'Unsuccessful error code: ' + res.statusCode));
+            }
+            else if (res.headers['content-type']
+                    .indexOf('application/json') !== 0) {
+				return cb(new Error(
+                        'application/json response type is expected'));
+            }
+			else if (!contentLength) {
+				return cb(new Error(
+                        'Respond had invalid content-length field'));
+            }
+
 			var d = ''
 			res.on('data', function(c) {
 				d = d + c;
@@ -50,15 +68,6 @@ var
 					return cb(null, JSON.parse(d));
 			});
 
-			if (res.statusCode / 100 !== 2)
-				return cb(new Error('Unsuccessful error code: ' + res.statusCode));
-
-			if (res.headers['content-type'].indexOf('application/json') !== 0)
-				return cb(new Error('application/json response type is expected'));
-
-			var contentLength = parseInt(res.headers['content-length']);
-			if (!contentLength)
-				return cb(new Error('Respond had invalid content-length field'));
 		});
 		r.on('error', cb);
 		r.write(d + '\n');
