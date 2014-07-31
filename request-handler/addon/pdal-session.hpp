91b75519d5cb00e27d54ee580e39a2c43d58a251
Connor Manning <connor@hobu.co>
2014-07-31 15:33:48 -0500

    Convert PDAL read to perform asynchronously.

--- request-handler/addon/pdal-session.hpp
+++ request-handler/addon/pdal-session.hpp
@@ -62,5 +62,39 @@ private:
     static v8::Handle<v8::Value> read(const v8::Arguments& args);
 
     std::shared_ptr<PdalInstance> m_pdalInstance;
+
+    struct ReadData
+    {
+        ReadData(
+                std::shared_ptr<PdalInstance> pdalInstance,
+                std::string host,
+                std::size_t port,
+                std::size_t start,
+                std::size_t count,
+                v8::Persistent<v8::Function> callback)
+            : pdalInstance(pdalInstance)
+            , host(host)
+            , port(port)
+            , start(start)
+            , count(count)
+            , data(0)
+            , numPoints(0)
+            , callback(callback)
+        { }
+
+        // Inputs
+        const std::shared_ptr<PdalInstance> pdalInstance;
+        const std::string host;
+        const std::size_t port;
+        const std::size_t start;
+        const std::size_t count;
+
+        // Outputs
+        unsigned char* data;
+        std::size_t numPoints;
+        std::size_t numBytes;
+
+        v8::Persistent<v8::Function> callback;
+    };
 };
 
