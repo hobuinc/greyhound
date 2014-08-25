9666414dc438dfe6da5c8bf5b05fc14462d462f6
Connor Manning <connor@hobu.co>
2014-08-24 21:11:12 -0500

    Plumb a Greyhound schema request and its handling into the C++ addon.

--- session-handler/addon/pdal-session.cpp
+++ session-handler/addon/pdal-session.cpp
@@ -5,22 +5,6 @@
 
 #include "pdal-session.hpp"
 
-namespace
-{
-    // TODO Client should specify these in their read request.
-    const pdal::Dimension::IdList dims(
-        {
-            pdal::Dimension::Id::X,
-            pdal::Dimension::Id::Y,
-            pdal::Dimension::Id::Z,
-            pdal::Dimension::Id::Intensity,
-            pdal::Dimension::Id::Red,
-            pdal::Dimension::Id::Green,
-            pdal::Dimension::Id::Blue
-        }
-    );
-}
-
 PdalSession::PdalSession()
     : m_pipelineManager()
     , m_pointBuffer()
@@ -93,26 +77,67 @@ std::string PdalSession::getDimensions() const
     return m_pointBuffer->context().dimsJson();
 }
 
-std::size_t PdalSession::getStride() const
-{
-    std::size_t stride(0);
-
-    for (const auto& dim : dims)
-    {
-        stride += pdal::Dimension::size(
-                pdal::Dimension::defaultType(dim));
-    }
-
-    return stride;
-}
-
 std::string PdalSession::getSrs() const
 {
     return m_pointBuffer->context().spatialRef().getRawWKT();
 }
 
+std::size_t PdalSession::readDim(
+        unsigned char* buffer,
+        const DimensionRequest& dim,
+        std::size_t index) const
+{
+    if (dim.type == "floating")
+    {
+        if (dim.size == 4)
+        {
+            float val(m_pointBuffer->getFieldAs<float>(dim.id, index));
+            std::memcpy(buffer, &val, dim.size);
+        }
+        else if (dim.size == 8)
+        {
+            double val(m_pointBuffer->getFieldAs<double>(dim.id, index));
+            std::memcpy(buffer, &val, dim.size);
+        }
+        else
+        {
+            throw std::runtime_error("Invalid floating size requested");
+        }
+    }
+    else
+    {
+        if (dim.size == 1)
+        {
+            uint8_t val(m_pointBuffer->getFieldAs<uint8_t>(dim.id, index));
+            std::memcpy(buffer, &val, dim.size);
+        }
+        else if (dim.size == 2)
+        {
+            uint16_t val(m_pointBuffer->getFieldAs<uint16_t>(dim.id, index));
+            std::memcpy(buffer, &val, dim.size);
+        }
+        else if (dim.size == 4)
+        {
+            uint32_t val(m_pointBuffer->getFieldAs<uint32_t>(dim.id, index));
+            std::memcpy(buffer, &val, dim.size);
+        }
+        else if (dim.size == 8)
+        {
+            uint64_t val(m_pointBuffer->getFieldAs<uint64_t>(dim.id, index));
+            std::memcpy(buffer, &val, dim.size);
+        }
+        else
+        {
+            throw std::runtime_error("Invalid integer size requested");
+        }
+    }
+
+    return dim.size;
+}
+
 std::size_t PdalSession::read(
         unsigned char** buffer,
+        const Schema& schema,
         const std::size_t start,
         const std::size_t count)
 {
@@ -131,14 +156,9 @@ std::size_t PdalSession::read(
 
         for (boost::uint32_t i(start); i < start + pointsToRead; ++i)
         {
-            for (const auto& dim : dims)
+            for (const auto& dim : schema)
             {
-                // TODO Type/size should come from client request.
-                m_pointBuffer->getRawField(dim, i, pos);
-
-                pos += pdal::Dimension::size(
-                        pdal::Dimension::defaultType(dim));
-
+                pos += readDim(pos, dim, i);
             }
         }
     }
@@ -152,6 +172,7 @@ std::size_t PdalSession::read(
 
 std::size_t PdalSession::read(
         unsigned char** buffer,
+        const Schema& schema,
         const double xMin,
         const double yMin,
         const double xMax,
@@ -192,13 +213,14 @@ std::size_t PdalSession::read(
             depthBegin,
             depthEnd));
 
-    std::size_t pointsRead = readIndexList(buffer, results);
+    std::size_t pointsRead = readIndexList(buffer, schema, results);
     m_quadIndex.reset(0);
     return pointsRead;
 }
 
 std::size_t PdalSession::read(
         unsigned char** buffer,
+        const Schema& schema,
         const bool is3d,
         const double radius,
         const double x,
@@ -217,14 +239,14 @@ std::size_t PdalSession::read(
     const std::vector<std::size_t> results(
             activeKdIndex->radius(x, y, z, radius * radius));
 
-    return readIndexList(buffer, results);
+    return readIndexList(buffer, schema, results);
 }
 
 std::size_t PdalSession::readIndexList(
         unsigned char** buffer,
+        const Schema& schema,
         const std::vector<std::size_t>& indexList)
 {
-
     const std::size_t pointsToRead(indexList.size());
 
     try
@@ -233,13 +255,9 @@ std::size_t PdalSession::readIndexList(
 
         for (std::size_t i : indexList)
         {
-            for (const auto& dim : dims)
+            for (const auto& dim : schema)
             {
-                // TODO Type/size should come from client request.
-                m_pointBuffer->getRawField(dim, i, pos);
-
-                pos += pdal::Dimension::size(
-                        pdal::Dimension::defaultType(dim));
+                pos += readDim(pos, dim, i);
             }
         }
     }
