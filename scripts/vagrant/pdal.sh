c8c44ae43fbecea225cc0b362ac610d08c62833a
Connor Manning <connor@hobu.co>
2015-01-01 17:12:44 -0600

    Get back on PDAL master.

--- scripts/vagrant/pdal.sh
+++ scripts/vagrant/pdal.sh
@@ -8,7 +8,7 @@ export NUMTHREADS
 
 git clone https://github.com/PDAL/PDAL.git pdal
 cd pdal
-git checkout quad
+git checkout b9b960a56ead0e997e0e715bf903714e2496d4be
 cmake   -G "Unix Makefiles" \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=/usr \
