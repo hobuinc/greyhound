27d0316914badc1e9159d77dbe01bd3bd913f448
Connor Manning <connor@hobu.co>
2015-02-03 16:38:48 -0600

    Use getField instead of getFieldAs to read dimensions.

--- session-handler/addon/types/dim-info.hpp
+++ session-handler/addon/types/dim-info.hpp
@@ -6,20 +6,19 @@
 
 struct DimInfo
 {
-    DimInfo(std::string name, std::string type, std::size_t size)
-        : id(pdal::Dimension::id(name)), type(type), size(size)
-    { }
-
     DimInfo(
             const pdal::Dimension::Id::Enum id,
             const pdal::Dimension::Type::Enum type)
         : id(id)
-        , type(pdal::Dimension::toName(pdal::Dimension::base(type)))
-        , size(pdal::Dimension::size(type))
+        , type(type)
     { }
 
     const pdal::Dimension::Id::Enum id;
-    const std::string type;
-    const std::size_t size;
+    const pdal::Dimension::Type::Enum type;
+
+    std::size_t size() const
+    {
+        return pdal::Dimension::size(type);
+    }
 };
 
