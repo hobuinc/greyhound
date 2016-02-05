72d772d0abec95fc8a903cf28098f7cea2c9e2a1
Connor Manning <connor@hobu.co>
2016-02-04 20:56:49 -0600

    Add authentication and caching mechanism.  Update config template documentation, and add HTTPS/auth sections.

--- controller/interfaces/http/handler.js
+++ controller/interfaces/http/handler.js
@@ -4,14 +4,17 @@ var
     https = require('https'),
 	path = require('path'),
     express = require('express'),
+    session = require('express-session'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     lessMiddleware = require('less-middleware'),
+    request = require('request'),
+    uuid = require('node-uuid'),
 
     console = require('clim')(),
 
-    controllerConfig = (require('../../../config').cn || { }),
-    httpConfig = (controllerConfig ? controllerConfig.http : { }),
+    config = (require('../../../config').cn || { }),
+    httpConfig = (config ? config.http : { }),
     accessControlString = 'Access-Control-Expose-Headers',
     exposedHeaders = (() => {
         if (httpConfig.headers && httpConfig.headers[accessControlString]) {
@@ -19,8 +22,25 @@ var
             delete httpConfig.headers[accessControlString];
             return h;
         }
-    })();
+    })()
     ;
+
+if (config.auth) {
+    var maybeAddSlashTo = (s) => s.slice(-1) == '/' ? s : s + '/';
+    config.auth.path = maybeAddSlashTo(config.auth.path);
+
+    if (!config.auth.secret) config.auth.secret = uuid.v4();
+
+    if (typeof config.auth.cacheMinutes == 'number') {
+        config.auth.cacheMinutes = {
+            good: config.auth.cacheMinutes,
+            bad: config.auth.cacheMinutes
+        };
+    }
+
+    config.auth.cacheMinutes.good = Math.max(config.auth.cacheMinutes.good, 1);
+    config.auth.cacheMinutes.bad= Math.max(config.auth.cacheMinutes.bad, 1);
+}
 
 http.globalAgent.maxSockets = 1024;
 
@@ -86,9 +106,81 @@ http.globalAgent.maxSockets = 1024;
     };
 
     var registerCommands = function(controller, app) {
+        if (config.auth) {
+            var auths = { };
+
+            app.use(session({
+                cookie: {
+                    path: '/',
+                    domain: config.auth.domain,
+                    httpOnly: true,
+                    secure: true,
+                    maxAge: 1000 * 60 * 60 * 24 * 365,
+                },
+                genid: () => uuid.v4(),
+                name: 'greyhound-user',
+                resave: false,
+                rolling: false,
+                saveUninitialized: false,
+                secret: config.auth.secret,
+                unset: 'keep'
+            }));
+
+            app.use(function(req, res, next) {
+                if (!req.session.greyhoundId) {
+                    req.session.greyhoundId = uuid.v4();
+                }
+
+                next();
+            });
+
+            app.use('/resource/:resource/*', function(req, res, next) {
+                var id = req.session.greyhoundId;
+                var resource = req.params.resource;
+
+                if (!auths[id]) auths[id] = { };
+
+                if (!auths[id][resource]) {
+                    console.log('Authing', id);
+                    var start = new Date();
+
+                    auths[id][resource] = new Promise((resolve, reject) => {
+                        var authPath = config.auth.path + resource;
+                        req.pipe(request(authPath, (err, authResponse) => {
+                            if (err) return reject(500);
+
+                            var code = authResponse.statusCode;
+                            var ok = Math.floor(code / 100) == 2;
+
+                            var time = (new Date() - start) / 1000;
+
+                            console.log('Authed', id, 'in', time, 's');
+
+                            var timeoutMs = ok ?
+                                config.auth.cacheMinutes.good :
+                                config.auth.cacheMinutes.bad;
+
+                            setTimeout(() => {
+                                delete auths[id][resource];
+                                if (!Object.keys(auths[id]).length) {
+                                    delete auths[id];
+                                }
+                            }, timeoutMs * 60000);
+
+                            if (ok) resolve();
+                            else reject(code);
+                        }));
+                    });
+                }
+
+                auths[id][resource].then(() => next())
+                .catch((statusCode) => res.status(statusCode).send());
+            });
+        }
+
         app.get('/resource/:resource/info', function(req, res) {
             controller.info(req.params.resource, function(err, data) {
-                if (err) return res.json(err.code || 500, err.message);
+                if (err) return res.status(err.code || 500).json(err.message);
                 else return res.json(data);
             });
         });
@@ -108,7 +200,7 @@ http.globalAgent.maxSockets = 1024;
                 function(err, data, done) {
                     if (err) {
                         console.error('Encountered data error');
-                        return res.json(err.code || 500, err.message);
+                        return res.status(err.code || 500).json(err.message);
                     }
 
                     res.write(data);
