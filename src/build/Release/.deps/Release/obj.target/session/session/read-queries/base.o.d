fd084e6642672fda044fdc7b0d062adbc463686c
Connor Manning <connor@hobu.co>
2016-09-23 11:42:52 -0400

    Ignore build directory.

--- src/build/Release/.deps/Release/obj.target/session/session/read-queries/base.o.d
+++
@@ -1,7 +0,0 @@
-cmd_Release/obj.target/session/session/read-queries/base.o := c++ '-DNODE_GYP_MODULE_NAME=session' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/connor/.node-gyp/4.4.6/include/node -I/Users/connor/.node-gyp/4.4.6/src -I/Users/connor/.node-gyp/4.4.6/deps/uv/include -I/Users/connor/.node-gyp/4.4.6/deps/v8/include -I../session -I/usr/include/jsoncpp  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-exceptions -fno-threadsafe-statics -std=c++11 -stdlib=libc++ -frtti -fexceptions -MMD -MF ./Release/.deps/Release/obj.target/session/session/read-queries/base.o.d.raw   -c -o Release/obj.target/session/session/read-queries/base.o ../session/read-queries/base.cpp
-Release/obj.target/session/session/read-queries/base.o: \
-  ../session/read-queries/base.cpp ../session/read-queries/base.hpp \
-  ../session/util/buffer-pool.hpp
-../session/read-queries/base.cpp:
-../session/read-queries/base.hpp:
-../session/util/buffer-pool.hpp:
