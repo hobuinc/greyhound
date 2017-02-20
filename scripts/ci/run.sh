34d1b71fcd9ffe553413a6916e56dfdfec670260
Connor Manning <connor@hobu.co>
2017-02-20 14:08:17 -0600

    Add Travis stuff.

---
+++ scripts/ci/run.sh
@@ -0,0 +1,9 @@
+docker run -t \
+    -v $TRAVIS_BUILD_DIR:/opt/greyhound \
+    --entrypoint /bin/bash \
+    connormanning/greyhound -c " \
+        NODE_ENV=debug cd /opt/greyhound && npm run generate-test-data && \
+        npm install && node-gyp configure && \
+        node-gyp build --debug && \
+        (NODE_ENV=debug nohup ./src/app.js --debug&) && sleep 10 && npm test"
+
--- scripts/generate-test-data.sh
+++ scripts/generate-test-data.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+mkdir -p $DIR/../data
 cd $DIR/../data
+mkdir -p tmp
 
 # Normally we'd glob here, but that's not possible with the HTTP interface.
 TESTING_SHALLOW=true entwine build \
