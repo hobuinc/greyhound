06df8ccef8a46cc2a3ef6d96eb174a14c1b88f90
Connor Manning <connor@hobu.co>
2017-08-02 20:14:37 -0500

    Add signal handler.

--- greyhound/main.cpp
+++ greyhound/main.cpp
@@ -1,8 +1,11 @@
+#include <csignal>
+
 #include <greyhound/app.hpp>
 #include <greyhound/configuration.hpp>
 
 int main(int argv, char** argc)
 {
+    signal(SIGINT, [](int sig) { exit(1); });
     greyhound::Configuration config(argv, argc);
     greyhound::App app(config);
     app.start();
